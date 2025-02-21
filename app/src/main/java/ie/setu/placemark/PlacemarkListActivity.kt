c47a6098e7a08b41e79f42de4bb5baed9bbc04de
Paul Dolan <paullydee0@gmail.com>
2025-02-21 16:57:24 +0000

    Created new empty views activity with additon of code to activity_placemark_list.xml and PLacemarkListActivity. AndroidManifest.xml was updated to utilise the additions of these files

---
+++ app/src/main/java/ie/setu/placemark/PlacemarkListActivity.kt
@@ -0,0 +1,17 @@
+package ie.setu.placemark.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.setu.placemark.R
+import ie.setu.placemark.main.MainApp
+
+class PlacemarkListActivity : AppCompatActivity() {
+
+    lateinit var app: MainApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placemark_list)
+        app = application as MainApp
+    }
+}
