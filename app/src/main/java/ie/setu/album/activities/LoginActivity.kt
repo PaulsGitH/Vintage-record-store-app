4518439360109d6418bee54df07533a514181a35
Paul Dolan <paullydee0@gmail.com>
2025-04-27 13:00:40 +0100

    Added LoginActivity class, updated HomeActivity to pass through to the login page, updated manifest to link to new menu to use login

---
+++ app/src/main/java/ie/setu/album/activities/LoginActivity.kt
@@ -0,0 +1,45 @@
+package ie.setu.album.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ie.setu.Album.R
+import ie.setu.Album.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            attemptLogin()
+        }
+
+        binding.passwordEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                attemptLogin()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun attemptLogin() {
+        val username = binding.usernameEditText.text.toString().trim()
+        val password = binding.passwordEditText.text.toString().trim()
+
+        if (username == "user" && password == "password") {
+            startActivity(Intent(this, AlbumListActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, getString(R.string.error_incorrect_login), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
